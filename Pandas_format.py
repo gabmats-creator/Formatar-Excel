c1db9e33fc253e4cbcbd59f14648e50ab0cd83ba
Gabriel Matheus Aguilera Silva <124635467+gabmats-creator@users.noreply.github.com>
2023-09-29 15:41:09 -0300

    initial commit

---
+++ Pandas_format.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import xlwt
+import tkinter as tk
+
+def obter_valores():
+    valor1 = entry1.get()
+    valor2 = entry2.get()
+# Ler os dados da planilha .xlsx existente em um DataFrame, trata os elementos nulos como strings vazias
+    dataframe = pd.read_excel(f'{valor1}.xlsx', header=None).fillna('')
+
+    # Cria um novo arquivo .xls e adiciona uma planilha a ele
+    new_file = f'/home/gabmats/Downloads/sinapi_08_2023/{valor2}.xls'
+    workbook = xlwt.Workbook(encoding='utf-8')
+    worksheet = workbook.add_sheet('Sheet 1')
+
+    # Escrever os dados do DataFrame na nova planilha
+    for row_index, row in enumerate(dataframe.values):
+        for col_index, cell_value in enumerate(row):
+            worksheet.write(row_index, col_index, cell_value)
+
+    # Salvar o arquivo .xls
+    workbook.save(new_file)
+    print('Arquivo salvo com sucesso')
+
+
+
+# Criar uma janela
+janela = tk.Tk()
+janela.title("Entrada de Dois Valores")
+
+# Criar rótulos e campos de entrada para os dois valores
+label1 = tk.Label(janela, text="Digite o primeiro valor:")
+label1.pack()
+
+entry1 = tk.Entry(janela)
+entry1.pack()
+
+label2 = tk.Label(janela, text="Digite o segundo valor:")
+label2.pack()
+
+entry2 = tk.Entry(janela)
+entry2.pack()
+
+# Criar um botão para confirmar a entrada dos valores
+botao = tk.Button(janela, text="Confirmar", command=obter_valores)
+botao.pack()
+
+# Iniciar a interface gráfica
+janela.mainloop()
+
+
+
